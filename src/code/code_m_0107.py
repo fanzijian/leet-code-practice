34b8c0896b1493d6111dcbffb23f3e5f4ac9f06c
fanzijian <fanzijian@baidu.com>
2020-04-07 22:42:15 +0800

    add 面试题01.07

---
+++ src/code/code_m_0107.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python
+#-*- coding: utf-8 -*-
+"""
+题目链接：https://leetcode-cn.com/problems/rotate-matrix-lcci/
+
+Authors: fanzijian(fanzijian@baidu.com)
+Date:    2020-04-06 11:23:56
+
+"""
+class Solution(object):
+    def rotate(self, matrix):
+        """
+        :type matrix: List[List[int]]
+        :rtype: None Do not return anything, modify matrix in-place instead.
+        """
+        n = len(matrix)
+        for i in range(n//2):
+            for j in range((n+1)//2):
+                matrix[i][j], matrix[j][n-i-1], matrix[n-i-1][n-j-1], matrix[n-j-1][i] = matrix[n-j-1][i], matrix[i][j], matrix[j][n-i-1], matrix[n-i-1][n-j-1]
+        return matrix
