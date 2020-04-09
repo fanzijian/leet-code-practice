1db329a55091b7e2f4e23fda5b86ce6213e4dca4
fanzijian <fanzijian@baidu.com>
2020-04-09 19:57:16 +0800

    add code_22 方法二

--- src/code/code_22.py
+++ src/code/code_22.py
@@ -1,9 +1,45 @@
 #!/usr/bin/env python
 #-*- coding: utf-8 -*-
+"""
+题目链接：https://leetcode-cn.com/problems/generate-parentheses/
+
+共有 l=n 个 '(' 以及 r=n 个 ')'，递归选择左括号或者右括号，当l==r==0时，添加到结果集合中，
+当保证每次选择，l>=r同时l>=0 且 r>=0
+
+Authors: fanzijian(fanzijian@baidu.com)
+Date:    2020-04-06 11:23:56
+
+"""
 class Solution(object):
     def generateParenthesis(self, n):
         """
         :type n: int
         :rtype: List[str]
         """
-        rst = []
+        if n <= 0:
+            return []
+
+        def genParenth(l, r, strs):
+            if l == 0 and r == 0:
+                return strs
+            if l == 0 and r != 0:
+                rst = []
+                for s in strs:
+                    s += ")" * r
+                    rst.append(s)
+                return rst
+            if l == r:
+                rst = []
+                for s in strs:
+                    s += "("
+                    rst.append(s)
+                return genParenth(l-1, r, rst)
+            if l < r:
+                rst1 = []
+                rst2 = []
+                for s in strs:
+                    rst1.append(s+"(")
+                    rst2.append(s+")")
+                return genParenth(l-1, r, rst1) + genParenth(l, r-1, rst2)
+
+        return genParenth(n, n, [""])
---
+++ src/code/code_22_beta.py
@@ -0,0 +1,34 @@
+#!/usr/bin/env python
+#-*- coding: utf-8 -*-
+"""
+题目链接：https://leetcode-cn.com/problems/generate-parentheses/
+
+可以考虑一个一个加符号，当数量达到2*n时，则表示达到给定长度
+期间维持l > r , l < n
+
+Authors: fanzijian(fanzijian@baidu.com)
+Date:    2020-04-06 11:23:56
+
+"""
+
+class Solution(object):
+    def generateParenthesis(self, n):
+        """
+        :type n: int
+        :rtype: List[str]
+        """
+        ans = []
+
+        def gen_parentesis(l, r, s):
+            if len(s) == 2 * n:
+                ans.append(s)
+            if l < n:
+                s += '('
+                gen_parentesis(l+1, r, s)
+                s = s[:-1]
+            if l > r:
+                s += ')'
+                gen_parentesis(l, r+1, s)
+                s = s[:-1]
+        gen_parentesis(0, 0, '')
+        return ans
